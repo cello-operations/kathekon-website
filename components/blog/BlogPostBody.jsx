b0b65864b419921a10a27b63f53f131b65a9b019
Tolulope Odueke <todueke415@nibss-plc.com.ng>
2020-11-28 15:13:50 +0100

    add share on linkedin button

--- components/blog/BlogPostBody.jsx
+++ components/blog/BlogPostBody.jsx
@@ -151,6 +151,10 @@ const BlogPostBody = ({ postObject, post, recentPosts }) => {
                         Tweet
                       </a>
                   </div>
+                  <div style={{ marginLeft: '1rem' }}>
+                    <script src="https://platform.linkedin.com/in.js" type="text/javascript">{`lang: en_US`}</script>
+                    <script type="IN/Share" data-url={`${hostURL}/blog/post/${postObject.slug}`}></script>
+                  </div>
                     <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
                  
                   <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
